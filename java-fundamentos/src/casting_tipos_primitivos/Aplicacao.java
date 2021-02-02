a47cfb06fbf8e99a4c519e4796d0f4f01efdb228
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-02 14:27:07 -0400

    Casting

---
+++ java-fundamentos/src/casting_tipos_primitivos/Aplicacao.java
@@ -0,0 +1,13 @@
+package casting_tipos_primitivos;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		// long x = 10;
+
+		// double x = 35;
+
+		float x = (float) 40.0;
+	}
+
+}
