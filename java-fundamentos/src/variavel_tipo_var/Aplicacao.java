040487b15ed885d4e7e182a131a122c337f83ca8
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-02 14:06:03 -0400

    Variavel tipo var

---
+++ java-fundamentos/src/variavel_tipo_var/Aplicacao.java
@@ -0,0 +1,12 @@
+package variavel_tipo_var;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		int x = 10;
+		var x2 = 20;
+		System.out.println(x + x2);
+		var y = true;
+	}
+
+}
