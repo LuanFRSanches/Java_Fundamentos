16e75e3225cb335d3639c3a9357fa79180ba6d10
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-03 16:40:26 -0400

    Construtores

---
+++ java-fundamentos/src/construtores/Aplicacao.java
@@ -0,0 +1,11 @@
+package construtores;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+
+		Retangulo r1 = new Retangulo();
+		double area1 = r1.calcularArea();
+		
+		System.out.println(area1);
+	}
+}
---
+++ java-fundamentos/src/construtores/Retangulo.java
@@ -0,0 +1,12 @@
+package construtores;
+
+public class Retangulo {
+
+	private double largura;
+	private double altura;
+
+	public double calcularArea() {
+		return largura * altura;
+	}
+
+}
