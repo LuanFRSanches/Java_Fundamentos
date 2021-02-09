22c7ef14b5451db42066a4e7f0dbfb6b649b3c61
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-08 20:45:27 -0400

    TextBlocks

--- java-fundamentos/src/teste/Exemplo.java
+++ java-fundamentos/src/teste/Exemplo.java
@@ -1,18 +1,25 @@
 package teste;
 
-public class Exemplo {
-	public static void main(String[] args) {
-		class Classe1 {
-			public void m() {
-				System.out.println(1);
-			}
-		}
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-		class Classe2 extends Classe1 {
-			public void m() {
-				super.m();
-				System.out.println(2);
-			}
+public class Exemplo {
+	public static void read() throws Exception {
+		try {
+			throw new IOException();
+		} catch (FileNotFoundException e) {
+			System.out.println("catch 1");
+			return;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		try {
+			read();
+		} catch (FileNotFoundException f) {
+			System.out.println("catch 2");
+		} finally {
+			System.out.println("finally");
 		}
 	}
 }
---
+++ java-fundamentos/src/textblocks/Main.java
@@ -0,0 +1,22 @@
+package textblocks;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * String html = "<html>\n"+ " <body>\n"+ "   <h1>Texto Qualquer<h1/>\n"+
+		 * " </body>\n"+ "</html>"; System.out.println(html);
+		 */
+
+		String html = """
+				<html>
+				 <body>
+				   <h1>Texto Qualquer<h1/>
+				 </body>
+				</html>
+
+				""";
+		System.out.println(html);
+	}
+
+}
