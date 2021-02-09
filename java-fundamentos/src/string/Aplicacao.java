c81cc648139c4fae38347120f313419f611130aa
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-08 21:16:28 -0400

    String

---
+++ java-fundamentos/src/string/Aplicacao.java
@@ -0,0 +1,22 @@
+package string;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		String s1 = new String("abc");
+		String s2 = "abc";
+		String s3 = "abc";
+
+		System.out.println(s1);
+		System.out.println(s2);
+
+		System.out.println(s1 == s2);
+
+		System.out.println(s2 == s3);
+
+		System.out.println(s1.equals(s2));
+
+		s2 = s2.toUpperCase();
+		s1 = s1.toLowerCase();
+	}
+}
