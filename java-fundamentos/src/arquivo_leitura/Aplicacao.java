82887f10391507305f137c41df81cc248b544b74
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-11 09:37:26 -0400

    Lendo arquivos

---
+++ java-fundamentos/src/arquivo_leitura/Aplicacao.java
@@ -0,0 +1,52 @@
+package arquivo_leitura;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Aplicacao {
+
+	public static void main(String[] args) throws IOException {
+
+		// try (InputStream is = new FileInputStream("entrada.txt")) {
+		//
+		// byte[] buffer = new byte[4];
+		//
+		// String s = "";
+		// int bytesLidos;
+		// while ((bytesLidos = is.read(buffer)) > -1) {
+		// s += new String(buffer, 0, bytesLidos);
+		// }
+		//
+		// String s = new String(buffer, 0, bytesLidos);
+		// System.out.println(s);
+		// System.out.println(s.length());
+		//
+
+		// try (BufferedReader reader = new BufferedReader(new
+		// FileReader("entrada.txt"))) {
+
+		// String s = "";
+		// String line;
+
+		// while ((line = reader.readLine()) != null) {
+		// s += line + "\n";
+		// }
+		// String line = reader.readLine();
+		// System.out.println(line);
+		// System.out.println(s);
+
+		// }
+
+		try (Scanner scanner = new Scanner(new File("entrada.txt"))) {
+			String s = "";
+			while (scanner.hasNextLine()) {
+
+				String line = scanner.nextLine();
+				s += line + "\n";
+
+			}
+			System.out.println(s);
+		}
+	}
+}
