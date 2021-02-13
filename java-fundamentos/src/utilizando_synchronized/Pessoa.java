f1f1aef3622a145c36f80b9a3fe308b81fc6923e
LuanFRSanches <luanfelipersanches@gmail.com>
2021-02-13 11:46:59 -0400

    Synchronized

---
+++ java-fundamentos/src/utilizando_synchronized/Pessoa.java
@@ -0,0 +1,26 @@
+package utilizando_synchronized;
+
+public class Pessoa implements Runnable {
+
+	private ContaBancaria contaBancaria;
+
+	public Pessoa(ContaBancaria contaBancaria) {
+		this.contaBancaria = contaBancaria;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			try {
+				contaBancaria.sacar(200);
+				Thread.sleep(100);
+
+				contaBancaria.depositar(200);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
